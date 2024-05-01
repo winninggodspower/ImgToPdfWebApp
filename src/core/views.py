04929c679bedc0c85797d34b5aa919ef3837b84d
winning godspower <winninggodspower@gmail.com>
2024-05-01 21:45:56 +0100

    reduce credit on each use

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, flash, request, redirect, send_file, current_app
+from flask_login import login_required, current_user
 import os
-from src import BASE_DIR
+from src import BASE_DIR, db
 
 from src.core.utils import MergeImageToPdf
 
@@ -9,8 +10,9 @@ core_blueprint = Blueprint("core", __name__)
 @core_blueprint.route("/")
 def home():
     return render_template("index.html")
-    
+
 @core_blueprint.route("/process", methods=["POST"])
+@login_required
 def process():
     if 'images' not in request.files:
         flash("No file uploaded")
@@ -19,11 +21,19 @@ def process():
     if not images:
         flash("no file uploaded")
 
+    elif current_user.credits < current_app.config['CREDIT_PER_USE']:
+        flash('you don\'t have enough credits. purhcase credit to complete this action', 'info')
+        
     else:
         pdf_path = os.path.join(current_app.config["PDF_FOLDER"], "merge.pdf")
         MergeImageToPdf(images, pdf_path) 
 
         pdf_filename = os.path.split(pdf_path)[1]
+
+        # reduce user credits
+        current_user.credits -= current_app.config['CREDIT_PER_USE']
+        db.session.commit()
+
         return redirect(f'/pdf/{pdf_filename}')
         
     return redirect("/")
