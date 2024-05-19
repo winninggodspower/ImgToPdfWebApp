a73684362bcc5e6cf5f0ff460f6a4020254eacc2
winning godspower <winninggodspower@gmail.com>
2024-05-19 21:57:22 +0100

    make alert dismissable

--- src/static/js/alert.js
+++ src/static/js/alert.js
@@ -6,25 +6,36 @@ let alertColors = {
     'error': 'alert-error',
 }
 
-let destroyAlert = (e) => {
-    button = e.target;
-    button.parentElement.parentElement.remove()
+function generateAlertId() {
+    return 'alert-' + Date.now();
 }
 
 export function Alert(message, type) {
-    // calling destory alert just so it will be added to build function
-    destroyAlert
+    const alertId = generateAlertId();
 
-    // type should be on of these => 'success', 'info', 'warning', 'error'
     let alert = document.createElement('div');
-    alert.classList.add('w-full', 'md:w-1/2', 'lg:w-1/3', 'fixed', 'top-0', 'left-0', 'right-0', 'mt-2', 'px-2', 'mx-auto', 'z-10')
+    alert.setAttribute('role', 'alert');
+    alert.classList.add('flex', 'items-center', 'p-4', 'mb-4', `text-${type}-800`, 'rounded-lg', `bg-${type}-50`, 'fixed', 'top-0', 'left-0', 'right-0', 'mt-2', 'px-2', 'mx-auto', 'z-10', 'w-full', 'md:w-1/2', 'lg:w-1/3', alertColors[type]);
+    alert.id = alertId;
+
     let alertContent = `
-        <div class="px-4 py-2 mb-4 ${alertColors[type]} border  rounded-md flex items-center justify-between">
-         <span>${message}</span>
-         <button type="button" onclick="destroyAlert()" class="ml-2 text-blue-500 hover: ${alertColors[type].text}" aria-label="Close">&times;</button>
-        </div>`
+        <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+        </svg>
+        <span class="sr-only">Info</span>
+        <div class="ms-3 text-sm font-medium">
+            ${message} <a href="#" class="font-semibold underline hover:no-underline">example link</a>.
+        </div>
+        <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-${type}-50 text-${type}-500 rounded-lg focus:ring-2 focus:ring-${type}-400 p-1.5 hover:bg-${type}-200 inline-flex items-center justify-center h-8 w-8 dark:hover:bg-gray-700" data-dismiss-target="#${alertId}" aria-label="Close">
+            <span class="sr-only">Close</span>
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+        </button>
+    `;
+
     alert.innerHTML = alertContent;
 
-    // append alert to body
-    document.body.appendChild(alert)
+    // Append alert to body
+    document.body.appendChild(alert);
 }
