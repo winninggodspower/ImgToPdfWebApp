49bd73a9f6950ec70309d054f6b65cef7ee043e9
winning godspower <winninggodspower@gmail.com>
2024-06-19 01:51:06 +0100

    add ci-cd workflow github action

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 from decouple import config
 
 DATABASE_URI = config("DATABASE_URL")
-# if DATABASE_URI.startswith("postgres://"):
-#     DATABASE_URI = DATABASE_URI.replace("postgres://", "postgresql://", 1)
+if DATABASE_URI.startswith("postgres://"):
+    DATABASE_URI = DATABASE_URI.replace("postgres://", "postgresql://", 1)
 
 class Config(object):
     DEBUG = False
@@ -24,3 +24,7 @@ class DevelopmentConfig(Config):
     DEBUG = True
     WTF_CSRF_ENABLED = False
     DEBUG_TB_ENABLED = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+    DEBUG_TB_ENABLED = False
