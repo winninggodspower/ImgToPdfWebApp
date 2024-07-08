c3e92d4cb4eded5e22e14abadc846acbfa1e4084
winning godspower <winninggodspower@gmail.com>
2024-07-09 00:33:24 +0100

    decode password

--- src/auth/models.py
+++ src/auth/models.py
@@ -16,7 +16,7 @@ class User(db.Model, UserMixin):
 
     def __init__(self, email, password, is_admin=False):
         self.email = email
-        self.password = bcrypt.generate_password_hash(password.encode('utf8'))
+        self.password = bcrypt.generate_password_hash(password).decode('utf8')
         self.created_on = datetime.now()
         self.is_admin = is_admin
 
